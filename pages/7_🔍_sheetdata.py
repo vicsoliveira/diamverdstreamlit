776a54c264405f1f1cafa534cfddb1f470236dff
vicsoliveira <116915472+vicsoliveira@users.noreply.github.com>
2023-03-07 21:17:40 -0300

    Update 7_🔍_sheetdata.py

--- pages/7_🔍_sheetdata.py
+++ pages/7_🔍_sheetdata.py
@@ -53,15 +53,15 @@ nome_vendas_sheet = vendas_sheet_u['nome'].tolist()
 idade_vendas_sheet = vendas_sheet_u['idade'].tolist()
 data_ult_vendas_sheet = vendas_sheet_u['data ped'].tolist()
 
-vendas_sheet_dat = vendas_sheet[['nome', 'data ped']].copy()
+vendas_sheet_dat = vendas_sheet.iloc[['nome', 'data ped']].copy()
 
-
-data_u = []
-for name in nome_vendas_sheet:
-         rows = vendas_sheet_dat.loc[vendas_sheet_dat['nome'] == name]
-         data_u.append(rows['data ped'].tolist(index = False))
+st.write(vendas_sheet_dat)
+# data_u = []
+# for name in nome_vendas_sheet:
+#          rows = vendas_sheet_dat.loc[vendas_sheet_dat['nome'] == name]
+#          data_u.append(rows['data ped'].tolist(index = False))
          
-st.write(data_u)
+# st.write(data_u)
 
 
 
